25f1b9bd342a3b5076e1f4387179084b36a2c55f
Israel Adetunji <adisen230@gmail.com>
2020-04-12 21:01:01 +0100

    Setup the frontend, landing page

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import React, { Fragment } from 'react';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
+  );
+}
+
+export default App;
