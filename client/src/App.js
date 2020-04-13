396d48978c9dc95c5135aa62d5a82b9c89d14396
Israel Adetunji <adisen230@gmail.com>
2020-04-13 16:50:20 +0100

    Redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,33 @@
 /** @format */
 
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
+
+// Redux stuff
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
